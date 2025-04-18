f5ef4a82c4142ce30e64d954ec57d8da58a28d95
tikam sah <tikamshah@outlook.com>
2025-04-18 17:23:15 +0530

    header

--- js/script.js
+++ js/script.js
@@ -33,9 +33,5 @@ function includeLayout(url,layout) {
         })
         .catch(error => console.error('Error fetching layout:', error));
 }
-
-includeLayout('https://it-tools.nepexo.com/header.html','headerContainer');
-
-includeLayout('https://it-tools.nepexo.com/footer.html','footerContainer');
- 
+ includeLayout('https://it-tools.nepexo.com/footer.html','footerContainer');
 
