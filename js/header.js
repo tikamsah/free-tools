1fed05bc1e408af1ef23d6aafb2ee64a0fb72adf
tikam sah <tikamshah@outlook.com>
2025-04-21 15:26:14 +0530

    dynamic header about

---
+++ js/header.js
@@ -0,0 +1,11 @@
+
+function includeHeaderLayout(url,layout) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(layout).innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching layout:', error));
+}
+includeHeaderLayout('https://it-tools.nepexo.com/header.html','headerContainer');
+
