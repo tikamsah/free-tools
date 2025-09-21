e051491a29d0516f66a7b04554e2e741c017a2f7
tikam sah <tikamshah@outlook.com>
2025-09-21 22:50:47 +0530

    conical scritt

--- js/header.js
+++ js/header.js
@@ -19,4 +19,20 @@ function includeLayout(url,layout) {
 }
 includeLayout('https://it-tools.roptive.com/footer.html','footerContainer');
 
+(function() {
+  // Get current path and query string
+  var path = window.location.pathname + window.location.search + window.location.hash;
+
+  // Build the canonical URL on the new domain
+  var canonicalUrl = "https://it-tools.roptive.com" + path;
+
+  // Create <link rel="canonical">
+  var link = document.createElement("link");
+  link.setAttribute("rel", "canonical");
+  link.setAttribute("href", canonicalUrl);
+
+  // Append to <head>
+  document.head.appendChild(link);
+})();
+
 
